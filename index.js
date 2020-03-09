65226411e4808eebea7a96d2a7f5f334d5ff7b9c
RareDrops <54132759+RareDrops@users.noreply.github.com>
2020-03-09 21:31:04 +1300

    Update index.js

--- index.js
+++ index.js
@@ -62,6 +62,8 @@ client.user.setActivity("with Mimi-chan");
 client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
 
 bot.on('guildMemberAdd', member => {
   console.log('User' + member.user.tag + 'has joined the server!');
@@ -73,6 +75,7 @@ bot.on('guildMemberAdd', member => {
 client.on("guildDelete", guild => {
   // this event triggers when the bot is removed from a guild.
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 
 // https://discordjs.guide/sequelize/#delta-adding-a-tag

1ec8d0d4bf12ee1e8621757ce83ef9d24ccfa691
RareDrops <54132759+RareDrops@users.noreply.github.com>
2020-09-15 17:19:50 +1200

    Update index.js

--- index.js
+++ index.js
@@ -285,7 +285,7 @@ client.on("message", async message => {
     message.channel.send({files: ["https://cdn.discordapp.com/attachments/640092288449642497/686695305034727432/EPvpufsVAAASm8d.png"]})
   } else
    if(message.content.toLowerCase()=== "a") {
-    message.channel.send({files: ["<:gura:755294983933657198>"]})
+    message.channel.send("<:gura:755294983933657198>")
   } else
   if (message.content.toLowerCase()=== "add mystery fucker"){
     try {
